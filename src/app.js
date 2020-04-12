dc33d97ce14abdc394f91753fb64196b35a5b63d
Karina Masalova <masalova.karina@mail.ru>
2020-04-13 02:48:39 +0300

    feat: add card class

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
-//import Cards from './cards';
+import Cards from './cards';
 import Component from './js/Component';
-//import Row from './js/Row';
-//import Column from './js/Column';
+import Row from './js/Row';
+import Column from './js/Column';
 
 window.addEventListener('load', () => {
   const container = new Component('div', null, 'container');
   document.body.append(container.element);
+
+  const row = new Row();
+
 });
---
+++ src/js/Card.js
@@ -0,0 +1,9 @@
+import Component from './Component';
+
+class Card extends Component {
+  constructor() {
+    super('div', null, 'card');
+  }
+}
+
+export default Card;
