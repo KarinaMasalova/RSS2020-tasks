d961445411404ecc4ff2dfedbc8439bfa3a96399
Karina Masalova <masalova.karina@mail.ru>
2020-04-13 19:12:11 +0300

    feat: create cards

--- src/app.js
+++ src/app.js
@@ -2,11 +2,27 @@ import Cards from './cards';
 import Component from './js/Component';
 import Row from './js/Row';
 import Column from './js/Column';
+import Card from './js/Card';
 
 window.addEventListener('load', () => {
   const container = new Component('div', null, 'container');
-  document.body.append(container.element);
-
   const row = new Row();
 
+  const category1 = Cards[0]; /* Action Set A' */
+  const categoryCards = category1.cards;
+  const createCardsFromObj = (obj) => {
+    const card = new Card(obj.word, obj.translation, obj.image, obj.audioSrc);
+    return card;
+  }
+
+  const cardComponents = categoryCards.map(createCardsFromObj);
+  const columnsWithCards = cardComponents.map( (cardComponent) => {
+    const column = new Column();
+    column.append(cardComponent);
+    return column;
+  });
+
+  row.append(...columnsWithCards);
+  container.append(row);
+  document.body.append(container.element);
 });
--- src/js/Component.js
+++ src/js/Component.js
@@ -4,6 +4,9 @@ class Component {
   constructor(tag, id, ...classes) {
     this.element = ElementCreator.createElement(tag, id, ...classes);
   }
+  append(...components) {
+    this.element.append(...components.map((c) => c.element));
+  }
 }
 
 export default Component;
