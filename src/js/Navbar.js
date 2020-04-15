04c2facd6e45a9610edd0523769d145ee5a6d825
Karina Masalova <masalova.karina@mail.ru>
2020-04-16 00:32:59 +0300

    feat: add main page link to menu

--- src/js/Navbar.js
+++ src/js/Navbar.js
@@ -5,12 +5,13 @@ import NavLink from './NavLink';
 class Navbar extends Component {
   constructor() {
     super('ul', null, 'navbar-nav');
+    const mainPage = new NavLink('Main page');
     const categories = Cards.map( (obj) => {
       const category = new NavLink(obj.category);
       this.append(category);
       return category;
     });
-    this.append(...categories);
+    this.append(mainPage, ...categories);
   }
 }
 
