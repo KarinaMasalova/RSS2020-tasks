2c4f3525240bc55ac4ff92e4a5f8a1399788652c
Karina Masalova <masalova.karina@mail.ru>
2020-04-16 02:40:46 +0300

    feat: replace word in card by click on menu

--- src/js/Card.js
+++ src/js/Card.js
@@ -8,12 +8,13 @@ class Card extends Component {
     super('div', null, 'card');
     this.cardImgTop = new CardImgTop(obj.image);
     this.cardText = new CardText(obj.word, obj.translation);
-    this.rotateBtn = new RotateBtn('./img/rotate.svg'); /* like in dist */
+    this.rotateBtn = new RotateBtn('./img/rotate.svg'); /* path like in dist */
     this.append(this.cardImgTop, this.cardText, this.rotateBtn);
   }
 
   replaceContent(cardObj) {
     this.cardImgTop.element.setAttribute('src', cardObj.image);
+    this.cardText.element.textContent = cardObj.word;
   }
 }
 
