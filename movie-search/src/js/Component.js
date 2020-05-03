7697562057d98c646a0b9e0c9e3eb5789c8c47fd
Karina Masalova <masalova.karina@mail.ru>
2020-05-03 20:24:19 +0300

    feat: add Component and ElementCreator classes

---
+++ movie-search/src/js/Component.js
@@ -0,0 +1,13 @@
+import ElementCreator from './ElementCreator';
+
+class Component {
+  constructor(tag, id, ...classes) {
+    this.element = ElementCreator.createElement(tag, id, ...classes);
+  }
+
+  append(...components) {
+    this.element.append(...components.map((c) => c.element));
+  }
+}
+
+export default Component;
