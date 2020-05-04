774b11860f204ba77760afcc85dbcdbaf87f281c
Karina Masalova <masalova.karina@mail.ru>
2020-05-04 16:15:31 +0300

    feat: add input search bar to the page

--- movie-search/src/app.js
+++ movie-search/src/app.js
@@ -1,6 +1,7 @@
 import Header from './js/Header';
 import HeaderTitle from './js/HeaderTitle';
 import SearchContainer from './js/SearchContainer';
+import InputSearchBar from './js/InputSearchBar';
 
 class App {
   constructor() {
@@ -8,6 +9,8 @@ class App {
     this.headerTitle = new HeaderTitle();
     this.header.append(this.headerTitle);
     this.searchContainer = new SearchContainer();
+    this.inputSearchBar = new InputSearchBar();
+    this.searchContainer.append(this.inputSearchBar);
     document.body.append(this.header.element, this.searchContainer.element);
   }
 }
---
+++ movie-search/src/js/InputSearchBar.js
@@ -0,0 +1,9 @@
+import Component from './Component';
+
+export default class InputSearchBar extends Component {
+  constructor() {
+    super('input', null, 'search-bar');
+    this.element.setAttribute('type', 'text');
+    this.element.setAttribute('placeholder','Search...');
+  }
+}
