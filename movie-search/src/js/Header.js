ee9a0ada1b7a4deb1fd780187674e8b5f1df69fb
Karina Masalova <masalova.karina@mail.ru>
2020-05-04 17:25:17 +0300

    refactor: change selector background name

--- movie-search/src/js/Header.js
+++ movie-search/src/js/Header.js
@@ -2,6 +2,6 @@ import Component from './Component';
 
 export default class Header extends Component {
   constructor() {
-    super('div', null, 'bg-header');
+    super('div', null, 'bg-gray');
   }
 }
