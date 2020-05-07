8460c2acc72fd07c3efae73903060b2cfce10b93
Karina Masalova <masalova.karina@mail.ru>
2020-05-07 03:04:17 +0300

    feat: append movie in slide

--- movie-search/src/js/SwiperSlide.js
+++ movie-search/src/js/SwiperSlide.js
@@ -3,5 +3,6 @@ import Component from './Component';
 export default class SwiperSlide extends Component {
   constructor(movie) {
     super('div', null, 'swiper-slide');
+    this.element.append(movie.element);
   }
 }
