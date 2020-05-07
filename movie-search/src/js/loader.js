0f1731493dd36bd84fecff8d3fdbd3ea85bf876e
Karina Masalova <masalova.karina@mail.ru>
2020-05-07 22:32:47 +0300

    refactor: delete console.log

--- movie-search/src/js/loader.js
+++ movie-search/src/js/loader.js
@@ -4,8 +4,7 @@ async function loadMovieData(query, page = 1) {
   const encoded = encodeURIComponent(query || 'dream');
   const url = `http://www.omdbapi.com/?s=${encoded}&page=${page}&apikey=${keyOMDb}`;
   const res = await fetch(url);
-  const data = await res.json(); 
-  console.log(data);
+  const data = await res.json();
   return data;
 }
 
