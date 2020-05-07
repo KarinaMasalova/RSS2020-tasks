e7c6507586cb37ba8d7ad353f8211a5c14fb0e90
Karina Masalova <masalova.karina@mail.ru>
2020-05-07 03:03:09 +0300

    refactor: add encodeURIComponent

--- movie-search/src/js/loader.js
+++ movie-search/src/js/loader.js
@@ -1,7 +1,8 @@
 const keyOMDb = '9514f3e9';
 
 async function loadMovieData(query, page = 1) {
-  const url = `http://www.omdbapi.com/?s=${query}&page=${page}&apikey=${keyOMDb}`;
+  const encoded = encodeURIComponent(query || 'dream');
+  const url = `http://www.omdbapi.com/?s=${encoded}&page=${page}&apikey=${keyOMDb}`;
   const res = await fetch(url);
   const data = await res.json(); 
   console.log(data);
