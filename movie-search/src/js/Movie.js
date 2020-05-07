d6b7ba368c0c31de92ee9f3b4bf94eab4b9ab256
Karina Masalova <masalova.karina@mail.ru>
2020-05-08 02:57:20 +0300

    feat: add movie rating

--- movie-search/src/js/Movie.js
+++ movie-search/src/js/Movie.js
@@ -2,14 +2,19 @@ import Component from './Component';
 import MovieTitle from './MovieTitle';
 import MoviePoster from './MoviePoster';
 import MovieYear from './MovieYear';
+import MovieRating from './MovieRating';
 
 export default class Movie extends Component {
   constructor(obj) {
     super('div', null, 'movie-container');
-    const { Title, Poster, Year } = obj;
+    const {
+      Title, Poster, Year, Rating,
+    } = obj;
     this.title = new MovieTitle(Title);
     this.poster = new MoviePoster(Poster);
     this.year = new MovieYear(Year);
-    this.element.append(this.title.element, this.poster.element, this.year.element);
+    this.rating = new MovieRating(Rating);
+    this.element.append(this.title.element, this.poster.element,
+      this.year.element, this.rating.element);
   }
 }
--- movie-search/src/js/loader.js
+++ movie-search/src/js/loader.js
@@ -1,11 +1,30 @@
 const keyOMDb = '9514f3e9';
 
+async function loadMovieRating(imdbID) {
+  const url = `http://www.omdbapi.com/?i=${imdbID}&apikey=${keyOMDb}`;
+  const result = await fetch(url);
+  const resJson = await result.json();
+  return resJson.imdbRating;
+}
+
 async function loadMovieData(query, page = 1) {
   const encoded = encodeURIComponent(query || 'dream');
   const url = `http://www.omdbapi.com/?s=${encoded}&page=${page}&apikey=${keyOMDb}`;
   const res = await fetch(url);
   const data = await res.json();
+
+  const addRating = async (obj) => {
+    const rate = await loadMovieRating(obj.imdbID);
+    Object.assign(obj, { Rating: rate });
+    return obj;
+  };
+
+  if (data.Response === 'True') {
+    await Promise.all(data.Search.map((obj) => addRating(obj)));
+    return data;
+  }
+
   return data;
 }
 
-export { loadMovieData };
+export default loadMovieData;
