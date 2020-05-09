4098afd6cd25960c9f57121b346e566220c8eaa3
Karina Masalova <masalova.karina@mail.ru>
2020-05-09 12:05:21 +0300

    feat: add star symbol before rating

--- movie-search/src/js/MovieRating.js
+++ movie-search/src/js/MovieRating.js
@@ -3,6 +3,6 @@ import Component from './Component';
 export default class MovieRating extends Component {
   constructor(rating) {
     super('p', null, 'movie-rating');
-    this.element.textContent = rating;
+    this.element.textContent = `★ ${rating}`;
   }
 }
