9e6d478f9f16bbb5c9401604270f658c48706729
Karina Masalova <masalova.karina@mail.ru>
2020-05-11 00:12:02 +0300

    feat: add getLang function

--- movie-search/src/js/loader.js
+++ movie-search/src/js/loader.js
@@ -1,4 +1,5 @@
 const keyOMDb = '9514f3e9';
+const yandexKey = 'trnsl.1.1.20200510T193539Z.bff37935debe481e.344c10072c40211dd1d8a5c7619e7466866eeadf';
 
 async function loadMovieRating(imdbID) {
   const url = `https://www.omdbapi.com/?i=${imdbID}&apikey=${keyOMDb}`;
@@ -7,17 +8,26 @@ async function loadMovieRating(imdbID) {
   return resJson.imdbRating;
 }
 
+const getLang = async (text) => {
+  const url = `https://translate.yandex.net/api/v1.5/tr.json/detect?key=${yandexKey}&text=${text}`;
+  const result = await fetch(url);
+  const json = await result.json();
+  console.log(json.lang);
+  return json.lang;
+};
+
+const addRating = async (obj) => {
+  const rate = await loadMovieRating(obj.imdbID);
+  Object.assign(obj, { Rating: rate });
+  return obj;
+};
+
 async function loadMovieData(query, page = 1) {
   const encoded = encodeURIComponent(query || 'dream');
+  /* сonst lang = await */ getLang(encoded);
   const url = `https://www.omdbapi.com/?s=${encoded}&page=${page}&apikey=${keyOMDb}`;
   const res = await fetch(url);
   const data = await res.json();
-
-  const addRating = async (obj) => {
-    const rate = await loadMovieRating(obj.imdbID);
-    Object.assign(obj, { Rating: rate });
-    return obj;
-  };
 
   if (data.Response === 'True') {
     await Promise.all(data.Search.map((obj) => addRating(obj)));
