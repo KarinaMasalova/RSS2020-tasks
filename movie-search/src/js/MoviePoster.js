636db935270fae7bdf86db287c7e9c036495f2f3
Karina Masalova <masalova.karina@mail.ru>
2020-05-10 21:32:28 +0300

    feat: make responsive layout

--- movie-search/src/js/MoviePoster.js
+++ movie-search/src/js/MoviePoster.js
@@ -4,5 +4,7 @@ export default class MoviePoster extends Component {
   constructor(poster) {
     super('img', null, 'movie-poster');
     this.element.setAttribute('src', poster);
+    this.element.setAttribute('width', '200');
+    this.element.setAttribute('height', '300');
   }
 }
