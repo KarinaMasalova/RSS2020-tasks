9329fa4d2eda7b40ee99cf12ca21c833001c2cda
Karina Masalova <masalova.karina@mail.ru>
2020-05-12 19:06:06 +0300

    fix: add preloader for movie poster

--- movie-search/src/js/MoviePoster.js
+++ movie-search/src/js/MoviePoster.js
@@ -5,7 +5,13 @@ export default class MoviePoster extends Component {
     super('div', null, 'movie-poster');
     this.noPoster = '../img/no-poster.jpg';
     this.url = (poster === 'N/A') ? this.noPoster : poster;
-    this.bgImage = `background-image: url('${this.url}')`;
-    this.element.setAttribute('style', this.bgImage);
+    /* this.bgImage = `background-image: url('${this.url}')`;
+    this.element.setAttribute('style', this.bgImage); */
+    this.preloaderImg = document.createElement('img');
+    this.preloaderImg.src = this.url;
+    this.preloaderImg.addEventListener('load', () => {
+      this.element.style.backgroundImage = `url(${this.url})`;
+      this.preloaderImg = null;
+    });
   }
 }
